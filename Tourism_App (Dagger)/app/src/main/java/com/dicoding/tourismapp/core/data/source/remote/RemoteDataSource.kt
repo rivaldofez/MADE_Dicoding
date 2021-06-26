000cf7cd42b5e5b1659aa8875b949120847cef22
rivaldofez <rivaldofez@gmail.com>
2021-06-27 04:22:29 +0700

    implement dagger on tourism app

---
+++ Tourism_App (Dagger)/app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,48 @@
+package com.dicoding.tourismapp.core.data.source.remote
+
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiResponse
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
+import com.dicoding.tourismapp.core.data.source.remote.response.ListTourismResponse
+import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class RemoteDataSource @Inject constructor(private val apiService: ApiService) {
+
+    @SuppressLint("CheckResult")
+    suspend fun getAllTourism(): Flow<ApiResponse<List<TourismResponse>>> {
+        //get data from remote api
+        return flow {
+            try {
+                val response = apiService.getList()
+                val dataArray = response.places
+                if (dataArray.isNotEmpty()){
+                    emit(ApiResponse.Success(response.places))
+                } else {
+                    emit(ApiResponse.Empty)
+                }
+            } catch (e : Exception){
+                emit(ApiResponse.Error(e.toString()))
+                Log.e("RemoteDataSource", e.toString())
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+
+}
